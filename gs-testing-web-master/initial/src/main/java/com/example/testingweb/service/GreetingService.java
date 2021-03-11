afbf9f0aba50fb93cb09ce87d6ad0f16898390fa
Shrunuti Singh <74618256+sunu09@users.noreply.github.com>
2021-03-10 18:10:35 -0600

    1st Commit

---
+++ gs-testing-web-master/initial/src/main/java/com/example/testingweb/service/GreetingService.java
@@ -0,0 +1,10 @@
+package com.example.testingweb.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String greet() {
+        return "Hello, World";
+    }
+}
